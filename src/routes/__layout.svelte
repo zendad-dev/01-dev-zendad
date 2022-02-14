a12fa6edd511d1b370df648ce378e0b6ee5c1445
Christophe R Patraldo <patrouch@gmail.com>
2022-02-13 20:45:25 -0500

    added vite imagetools

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,8 @@
+<script>          
+    import Trademark from '$img/PinchePoutineTrademark.jpg'
+    import Logo1 from '$img/SmokinGunORIG.webp'
+ </script>          
+ 
+ <img src={Trademark} alt="Pinche Poutine Logo">
+ <img src={Logo1} alt="Claire Voyant">
+ 
--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,8 @@
-import adapter from '@sveltejs/adapter-auto';
+import * as path from 'path';
+import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
+import { defineConfig } from 'vite'
+import { imagetools } from 'vite-imagetools'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,7 +11,30 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			out: 'build',
+			precompress: false,
+			env: {
+				path: 'SOCKET_PATH',
+				host: 'HOST',
+				port: 'PORT',
+				origin: 'ORIGIN',
+				headers: {
+					protocol: 'PROTOCOL_HEADER',
+					host: 'HOST_HEADER'
+				}
+			}
+		}),
+		trailingSlash: 'always',
+		vite: {
+			plugins: [imagetools],
+			resolve: {
+				alias: {
+					$img: path.resolve('src/images')
+				}
+			}
+		}
 	}
 };
 
