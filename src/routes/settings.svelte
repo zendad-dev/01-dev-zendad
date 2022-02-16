976c12adb957a2111afae6642049f3af493be0fa
Christophe R Patraldo <patrouch@gmail.com>
2022-02-16 13:46:59 -0500

    finally got MtlMetro.svg without error

--- src/routes/settings.svelte
+++ src/routes/settings.svelte
@@ -1,2 +1,6 @@
 <!-- src/routes/settings.svelte -->
+<script lang='ts'>
+import MtlGreen from '/static/images/Montreal_Metro_Logo-Green.svg';
+</script>
+<img src={MtlGreen} alt="Montreal Metro Sign">
 <h1>Settings</h1>
