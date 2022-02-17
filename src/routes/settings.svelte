506f679ed02baebe3e9edbb6eefa968157022cd0
Christophe R Patraldo <patrouch@gmail.com>
2022-02-16 21:25:07 -0500

    still working on basics svg

--- src/routes/settings.svelte
+++ src/routes/settings.svelte
@@ -1,6 +1,20 @@
 <!-- src/routes/settings.svelte -->
 <script lang='ts'>
 import MtlGreen from '/static/images/Montreal_Metro_Logo-Green.svg';
+import MtlBlue from '/static/Montreal_Metro_Logo.svg';
 </script>
-<img src={MtlGreen} alt="Montreal Metro Sign">
-<h1>Settings</h1>
+
+
+
+<img src="{MtlGreen}" alt="Montreal Metro Sign Green">
+<img src="{MtlBlue}" alt="Montreal Metro Sign Blue">
+
+<style>
+	:global(svg) {
+		max-width: 400px;
+		display: block
+	}
+</style>
+<!--<img src={MtlBlue} alt="Montreal Metro Blue">
+<img src={MtlGreen} alt="Montreal Metro Green">
+ <h1>Settings</h1> -->
