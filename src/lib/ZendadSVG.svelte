d2ec4764e669a196b0a780bf3b812425cac2aabb
Christophe R Patraldo <patrouch@gmail.com>
2022-03-06 22:27:52 -0500

    toying with css for the navbar

---
+++ src/lib/ZendadSVG.svelte
@@ -0,0 +1,10 @@
+<svg version="1.1"
+     width="300" height="200">
+
+  <rect width="100%" height="100%" fill="#34515e" />
+
+  <circle cx="150" cy="100" r="80" fill="#0c0000" />
+
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="#ffffff">ZenDad</text>
+
+</svg>
