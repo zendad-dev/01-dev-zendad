bf08e7de686b673d52d5675ea0899a40905cefaf
Christophe R Patraldo <patrouch@gmail.com>
2022-03-12 09:23:56 -0500

    playing with font

--- src/lib/ZendadSVG.svelte
+++ src/lib/ZendadSVG.svelte
@@ -5,6 +5,7 @@
 
   <circle cx="150" cy="100" r="80" fill="#333333" />
 
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="#ffffff">ZenDad</text>
+  <text x="150" y="125" font-family="Comic Sans MS, italic" font-size="60" text-anchor="middle" fill="#ffffff">ZenDad</text>
+  <text x="100" y="20" font-family="monospace" font-size="20" text-anchor="bottom-right" fill="#ffffff">Moonspace</text>
 
 </svg>
--- src/routes/settings.svelte
+++ src/routes/settings.svelte
@@ -10,9 +10,17 @@ import MtlBlue from '/static/Montreal_Metro_Logo.svg';
 <img src="{MtlBlue}" alt="Montreal Metro Sign Blue">
 
 <style>
+	@font-face {
+        font-family: 'Gelasio';
+        font-style: normal;
+        font-weight: 400;
+        src: local('Gelasio Regular'), local('Gelasio-Regular'), url(https://fonts.gstatic.com/s/gelasio/v1/cIf9MaFfvUQxTTqS9C6hYQ.woff2) format('woff2');
+        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+    }
 	:global(svg) {
 		max-width: 400px;
-		display: block
+		display: block;
+		font-family: Gelasio
 	}
 </style>
 <!--<img src={MtlBlue} alt="Montreal Metro Blue">
